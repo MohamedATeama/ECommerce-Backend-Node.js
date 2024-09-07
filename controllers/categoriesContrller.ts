6672e8610c0a5b4cd94ebb506b9189dee68a63fd
mohamed teama <MohamedATeama>
2024-09-08 02:01:36 +0300

    task3

--- controllers/categoriesContrller.ts
+++ controllers/categoriesContrller.ts
@@ -1,29 +1,13 @@
-import { NextFunction, Request, Response } from "express";
-import expressAsyncHandler from "express-async-handler";
 import { Categories } from "../interfaces/categoriesInterface";
 import categoriesmodel from "../models/categoriesModel";
+import { createOne, deleteOne, getAll, getOne, updateOne } from './refactorHandling';
 
-export const getAllCategories = expressAsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  const categories: Categories[] = await categoriesmodel.find();
-  res.status(200).json({ data: categories })
-});
+export const getAllCategories = getAll<Categories>(categoriesmodel, 'categories');
 
-export const createCategory = expressAsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  const category: Categories = await categoriesmodel.create(req.body);
-  res.status(201).json({ data: category })
-});
+export const createCategory = createOne<Categories>(categoriesmodel);
 
-export const getCategory = expressAsyncHandler(async (req:Request, res: Response, next:NextFunction) => {
-  const category: Categories | null = await categoriesmodel.findById(req.params.id);
-  res.status(200).json({ data: category })
-});
+export const getCategory = getOne<Categories>(categoriesmodel);
 
-export const updateCategory = expressAsyncHandler(async (req:Request, res:Response, next: NextFunction) => {
-  const category: Categories | null = await categoriesmodel.findByIdAndUpdate(req.params.id, req.body, {new: true});
-  res.status(200).json({ data: category })
-});
+export const updateCategory = updateOne<Categories>(categoriesmodel);
 
-export const deleteCategory = expressAsyncHandler(async (req:Request, res:Response, next: NextFunction) => {
-  const category: Categories | null = await categoriesmodel.findByIdAndDelete(req.params.id);
-  res.status(204).json()
-});
+export const deleteCategory = deleteOne<Categories>(categoriesmodel);
--- routes/subCategoriesRoute.ts
+++ routes/subCategoriesRoute.ts
@@ -1,15 +1,24 @@
 import { Router } from "express";
 import { createSubCategory, deleteSubCategory, getAllSubCategories, getSubCategory, updateSubCategory } from "../controllers/subCategoriesControllers";
+import { filterSubCategories } from "./../controllers/subCategoriesControllers";
+import {
+  createSubCategoryValidator,
+  getSubCategoryValidator,
+  updateSubCategoryValidator,
+  deleteSubCategoryValidator,
+} from "../utils/validator/subCategoriesValidator";
 
-const subCategoriesRoute: Router = Router();
+const subCategoriesRoute: Router = Router({ mergeParams: true });
 
-subCategoriesRoute.route("/")
-.get(getAllSubCategories)
-.post(createSubCategory);
+subCategoriesRoute
+  .route("/")
+  .get(filterSubCategories, getAllSubCategories)
+  .post(createSubCategoryValidator, createSubCategory);
 
-subCategoriesRoute.route("/:id")
-.get(getSubCategory)
-.put(updateSubCategory)
-.delete(deleteSubCategory);
+subCategoriesRoute
+  .route("/:id")
+  .get(getSubCategoryValidator, getSubCategory)
+  .put(updateSubCategoryValidator, updateSubCategory)
+  .delete(deleteSubCategoryValidator, deleteSubCategory);
 
 export default subCategoriesRoute;
